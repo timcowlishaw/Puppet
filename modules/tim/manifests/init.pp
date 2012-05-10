3d3f1ef3e46bd864c54089fe54e0ae1223f056c6
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-05-10 13:54:59 +0100

    turn bash_profile into a template for per-host prompt colouring

--- modules/tim/manifests/init.pp
+++ modules/tim/manifests/init.pp
@@ -7,8 +7,19 @@ class tim {
     managehome => "true",
   }
 
+  if $hostname == "motueka" {
+    $prompt_colour_1 = 'R'
+    $prompt_colour_2 = 'C'
+  } elsif $hostname == "riwaka" {
+    $prompt_colour_1 = 'Y'
+    $prompt_colour_2 = 'G'
+  } else {
+    $prompt_colour_1 = 'W'
+    $prompt_colour_2 = 'W'
+  }
+
   file {"/home/tim/.bash_profile":
-    source => "/etc/puppet/modules/tim/files/bash_profile",
+    content => template("tim/bash_profile.erb"),
     owner => "tim",
     group => "tim",
     mode => "640",
