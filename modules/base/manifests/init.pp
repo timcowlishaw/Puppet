9b66c04f4be4236e72eb5320d293d2cddde7c8cf
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-06-03 16:46:47 +0100

    fix require ruby from base

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,7 +1,9 @@
 class base {
+  require => Class['ruby']
   package { "build-essential": ensure => "installed" }
   package { "bash-completion": ensure => "installed" }
   package { "git-core": ensure => "installed" }
   package { "puppet": ensure => "installed" }
   package { "wget": ensure => "installed" }
 }
+
