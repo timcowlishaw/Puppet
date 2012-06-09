a87bfd98786613c66dba177b3fda38287e82f967
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-06-09 13:17:34 +0100

    use apt plugin to manage repos

--- modules/base/manifests/apt-defaults.pp
+++ modules/base/manifests/apt-defaults.pp
@@ -1,19 +1,65 @@
 class apt-defaults {
-  file {"/etc/apt/preferences.d/default":
-    content => "puppet:///modules/base/apt-preferences-default",
-    owner => "root",
-    group => "root",
-    mode => "644"
+  class { 'apt':
+    always_apt_update => true,
+    purge_sources_list => true,
+    purge_sources_list_d => true,
+    purge_preferences_d => true
   }
 
-  file {"/etc/apt/sources.list.d/sid":
-    content => "puppet:///modules/base/apt-sources-sid",
-    owner => "root",
-    group => "root",
-    mode => "644"
+  class {'apt::release':
+    release_id => 'stable'
+  }
+  apt::pin {'stable-updates': release => 'stable/updates', priority => 1000 }
+  apt::pin {'testing-updates': release => 'testing/updates', priority => 1000 }
+  apt::pin {'stable' : priority => 995 }
+  apt::pin {'testing': priority => 750 }
+  apt::pin {'unstable': priority => 50 }
+  apt::pin {'experimental': priority => 1}
+
+  apt::source {'stable-updates':
+    location => "http://security.debian.org",
+    release => "stable/updates",
+    repos => "main contrib non-free",
+    pin => 1000
   }
 
-  exec {"apt-update":
-    command => "/usr/bin/apt-get update"
+  apt::source {'testing-updates':
+    location => "http://security.debian.org",
+    release => "testing/updates",
+    repos => "main contrib non-free",
+    pin => 1000
   }
+
+  apt::source {'stable':
+    location => "http://ftp.uk.debian.org/debian",
+    release => "stable",
+    repos => "main contrib non-free",
+    pin => 995,
+    include_src => true
+  }
+
+  apt::source {'testing':
+    location => "http://ftp.uk.debian.org/debian",
+    release => "testing",
+    repos => "main contrib non-free",
+    pin => 750,
+    include_src => true
+  }
+
+  apt::source {'unstable':
+    location => "http://ftp.uk.debian.org/debian",
+    release => "unstable",
+    repos => "main contrib non-free",
+    pin => 50,
+    include_src => true
+  }
+
+  apt::source {'experimental':
+    location => "http://ftp.uk.debian.org/debian",
+    release => "unstable",
+    repos => "main contrib non-free",
+    pin => 1,
+    include_src => true
+  }
+
 }
