fdfa8f9f97e0e5e266738bdea36e88283b41dce8
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-06-09 13:24:38 +0100

    oops! we do want the pin directives after all

--- modules/base/manifests/apt-defaults.pp
+++ modules/base/manifests/apt-defaults.pp
@@ -9,26 +9,29 @@ class apt-defaults {
   class {'apt::release':
     release_id => 'stable'
   }
+  apt::pin {'stable-updates': release => 'stable/updates', priority => 1000 }
+  apt::pin {'testing-updates': release => 'testing/updates', priority => 1000 }
+  apt::pin {'stable' : priority => 995 }
+  apt::pin {'testing': priority => 750 }
+  apt::pin {'unstable': priority => 50 }
+  apt::pin {'experimental': priority => 1}
 
   apt::source {'stable-updates':
     location => "http://security.debian.org",
     release => "stable/updates",
     repos => "main contrib non-free",
-    pin => 1000
   }
 
   apt::source {'testing-updates':
     location => "http://security.debian.org",
     release => "testing/updates",
     repos => "main contrib non-free",
-    pin => 1000
   }
 
   apt::source {'stable':
     location => "http://ftp.uk.debian.org/debian",
     release => "stable",
     repos => "main contrib non-free",
-    pin => 995,
     include_src => true
   }
 
@@ -36,7 +39,6 @@ class apt-defaults {
     location => "http://ftp.uk.debian.org/debian",
     release => "testing",
     repos => "main contrib non-free",
-    pin => 750,
     include_src => true
   }
 
@@ -44,7 +46,6 @@ class apt-defaults {
     location => "http://ftp.uk.debian.org/debian",
     release => "unstable",
     repos => "main contrib non-free",
-    pin => 50,
     include_src => true
   }
 
@@ -52,7 +53,6 @@ class apt-defaults {
     location => "http://ftp.uk.debian.org/debian",
     release => "unstable",
     repos => "main contrib non-free",
-    pin => 1,
     include_src => true
   }
 
