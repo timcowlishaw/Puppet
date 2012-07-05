b0ff5d495f1f3fc2565bc76a352eb64885b4e154
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-07-05 16:50:39 +0100

    correct urxvt package name

--- modules/x/manifests/init.pp
+++ modules/x/manifests/init.pp
@@ -6,7 +6,7 @@ class x {
   package { "xorg": ensure => "installed" }
   package { "xmonad": ensure => "installed" }
   package { "trayer": ensure => "installed" }
-  package { "urxvt": ensure => "installed" }
+  package { "rxvt-unicode-256color": ensure => "installed" }
   package { "parcellite": ensure => "installed" }
   package { "dmenu": ensure => "installed"}
 }
