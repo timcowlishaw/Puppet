58e0588ec6aa75e8bed5f22858adf3785eabb98d
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-07-05 16:47:59 +0100

    some extra x stuff

--- modules/x/manifests/init.pp
+++ modules/x/manifests/init.pp
@@ -4,4 +4,9 @@ class x {
   }
 
   package { "xorg": ensure => "installed" }
+  package { "xmonad": ensure => "installed" }
+  package { "trayer": ensure => "installed" }
+  package { "urxvt": ensure => "installed" }
+  package { "parcellite": ensure => "installed" }
+  package { "dmenu": ensure => "installed"}
 }
