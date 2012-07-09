4eb9dd2e929f0ffa03cd5cb0dbaf514a0495590d
root <root@cascade.playlouder.com>
2012-07-09 11:56:55 +0100

    add ruby 1.9 and set alternatives

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -8,5 +8,17 @@ class ruby {
   package {'ruby1.8':
     ensure => "installed",
   }
+  package {'ruby1.9.1':
+    ensure => "installed",
+  }
+  package {'ruby1.9.1-dev':
+    ensure => "installed",
+  }
+  exec {"/usr/bin/update-alternatives --set ruby /usr/bin/ruby1.8":
+    require  => Package['ruby1.8', 'ruby1.9.1', 'ruby1.8-dev', 'ruby1.9.1-dev', 'rubygems1.8'],
+  }
+  exec {"/usr/bin/update-alternatives --set gem /usr/bin/gem1.8":
+    require  => Package['ruby1.8', 'ruby1.9.1', 'ruby1.8-dev', 'ruby1.9.1-dev', 'rubygems1.8'],
+  }
   Class['base'] -> Class['ruby']
 }
