8020ddb0a70e6f5fe0da9605027c3727ba321dec
root <root@cascade.playlouder.com>
2012-07-10 11:39:07 +0100

    node config

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -12,70 +12,36 @@ node 'cascade' inherits default {
   include comms
   include audio
   include spotify
-}
-
-node 'riwaka' inherits default {
-  #include rvm
-  #include gui
-  #include vim
-  #include rythmbox
-  #include spotify
-  #include dropbox
-  #include fonts
-  #include libreoffice
-  #include ack
-  #include chromium
-  #include mendeley
-  #include virtualbox
-  #include git
-  #include gmpc
-  #include wifi
-  #include printing
-  #include mysql
-  #include sqlite
-  #pdf reader
-  #mail client
-  #chat client
-  #terminal
+  include mysql
+  include rvm
 }
 
 node 'motueka' inherits default {
- # include base
+  include comms
+  include rvm
+  include x
+  include audio
+  include spotify
+  include mysql
  # include ssd
- # include ssh
- # include sudo
- # include tim
- # include ruby
- # include rvm
  # include python
  # include haskell
- # include gui
- # include vim
  # include rythmbox
  # include vlc
- # include spotify
  # include dropbox
- # include fonts
  # include java
  # include shotwell
  # include libreoffice
- # include ack
- # include chromium
  # include mendeley
  # include virtualbox
- # include git
  # include gimp
  # include gmpc
  # include wifi
  # include printing
- # include mysql
  # include postgresql
  # include sqlite
  # include redis
  # include skype
  # include transmission
   #pdf reader
-  #mail client
-  #chat client
-  #terminal
 }
