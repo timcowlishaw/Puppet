83e11edcde32449e98553dac0adf136bbbf1c6d5
root <root@motueka>
2012-07-25 11:44:20 +0100

    virtualbox on motueka. get rid of some comment ccruft

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -29,24 +29,19 @@ node 'motueka' inherits default {
   include thinkpad-power
   include disable-bell
   include python-dev
+  include virtualbox
  # include ssd
  # include haskell
  # include rythmbox
- # include vlc
- # include dropbox
  # include java
  # include shotwell
- # include libreoffice
  # include mendeley
- # include virtualbox
  # include gimp
  # include gmpc
- # include wifi
  # include printing
  # include postgresql
  # include sqlite
  # include redis
  # include skype
  # include transmission
-  #pdf reader
 }
