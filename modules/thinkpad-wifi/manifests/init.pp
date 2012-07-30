285c88068603b5afe918620d01a1455e9bb7a93e
root <root@motueka>
2012-07-30 17:29:56 +0100

    add wifi unblocking script

--- modules/thinkpad-wifi/manifests/init.pp
+++ modules/thinkpad-wifi/manifests/init.pp
@@ -2,4 +2,11 @@ class thinkpad-wifi {
   package {"firmware-iwlwifi": ensure => "installed"}
   package {"rfkill": ensure => "installed"}
   package {"wicd": ensure => "installed"}
+  kmod::blacklist {"acer-wmi": }
+  file {"/etc/rc.local.d/50-unblock-wifi":
+    ensure => "present",
+    source => "puppet:///modules/thinkpad-wifi/50-unblock-wifi",
+    mode => "700"
+  }
+  Class["base"] -> Class["thinkpad-wifi"]
 }
