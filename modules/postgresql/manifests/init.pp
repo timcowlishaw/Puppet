a23d0ba254929bdd5ca6f36913a7cb01ae3c6c56
root <root@motueka>
2012-11-19 11:11:43 +0000

    use md5 auth strategy for postgres

--- modules/postgresql/manifests/init.pp
+++ modules/postgresql/manifests/init.pp
@@ -2,4 +2,11 @@ class postgresql {
   package {'postgresql': ensure => 'installed'}
   package {'postgresql-client': ensure => 'installed'}
   package {'libpq-dev': ensure => 'installed'}
+  augeas { "allow-postgres-login-without-unix-user-account":
+    context => "/files/etc/postgresql/*/*/pg_hba.conf/*[type='local' and database='all' and user='all']",
+    changes => [
+      "set ./method md5"
+    ],
+    require => Package['postgresql']
+  }
 }
